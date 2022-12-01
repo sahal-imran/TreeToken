cdada1e702a6b0cc8d6e7dc18a9e4e973dbaf94c
sahal-imran <sahalimran786@gmail.com>
2022-12-01 23:58:59 +0500

    8

--- components/Picture/Gallery.jsx
+++ components/Picture/Gallery.jsx
@@ -1,40 +1,7 @@
 import React from 'react';
 import ImageGallery from 'react-image-gallery';
 
-// const images = [
-//     {
-//         original: '/assets/1.jpg',
-//         thumbnail: '/assets/1.jpg',
-//     },
-//     {
-//         original: '/assets/2.jpg',
-//         thumbnail: '/assets/2.jpg',
-//     },
-//     {
-//         original: '/assets/3.jpg',
-//         thumbnail: '/assets/3.jpg',
-//     },
-//     {
-//         original: '/assets/4.jpg',
-//         thumbnail: '/assets/4.jpg',
-//     },
-//     {
-//         original: '/assets/5.jpg',
-//         thumbnail: '/assets/5.jpg',
-//     },
-//     {
-//         original: '/assets/6.jpg',
-//         thumbnail: '/assets/6.jpg',
-//     },
-//     {
-//         original: '/assets/7.jpg',
-//         thumbnail: '/assets/7.jpg',
-//     },
-//     {
-//         original: '/assets/8.jpg',
-//         thumbnail: '/assets/8.jpg',
-//     },
-// ];
+
 
 function Gallery() {
     return (
@@ -52,19 +19,35 @@ export default Gallery;
 
 const images = [
     {
-        original: 'img/About/slide1.png',
-        thumbnail: 'img/About/t1.png',
+        original: 'img/Gallery/1.jpg',
+        thumbnail: 'img/Gallery/1.jpg',
     },
     {
-        original: 'img/About/slide1.png',
-        thumbnail: 'img/About/t2.png',
+        original: 'img/Gallery/2.jpg',
+        thumbnail: 'img/Gallery/2.jpg',
     },
     {
-        original: 'img/About/slide1.png',
-        thumbnail: 'img/About/t1.png',
+        original: 'img/Gallery/3.jpg',
+        thumbnail: 'img/Gallery/3.jpg',
     },
     {
-        original: 'img/About/slide1.png',
-        thumbnail: 'img/About/t2.png',
+        original: 'img/Gallery/4.jpg',
+        thumbnail: 'img/Gallery/4.jpg',
+    },
+    {
+        original: 'img/Gallery/5.jpg',
+        thumbnail: 'img/Gallery/5.jpg',
+    },
+    {
+        original: 'img/Gallery/6.jpg',
+        thumbnail: 'img/Gallery/6.jpg',
+    },
+    {
+        original: 'img/Gallery/7.jpg',
+        thumbnail: 'img/Gallery/7.jpg',
+    },
+    {
+        original: 'img/Gallery/8.jpg',
+        thumbnail: 'img/Gallery/8.jpg',
     },
 ];
